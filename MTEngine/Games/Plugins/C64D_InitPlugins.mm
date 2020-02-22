d350196721362a82407ce200640d9082e444e1f6
Marcin Skoczylas <slajerek@gmail.com>
2020-02-22 11:53:45 +0100

    v0.64.58

--- MTEngine/Games/Plugins/C64D_InitPlugins.mm
+++ MTEngine/Games/Plugins/C64D_InitPlugins.mm
@@ -2,19 +2,11 @@
 #include "CViewC64.h"
 
 #include "C64DebuggerPluginDummy.h"
-#include "C64DebuggerPluginSpiral.h"
-#include "C64DebuggerPluginShowPic.h"
 
 void C64D_InitPlugins()
 {
 //	C64DebuggerPluginDummy *plugin = new C64DebuggerPluginDummy();
 //	viewC64->RegisterEmulatorPlugin(plugin);
 	
-//	C64DebuggerPluginSpiral *plugin = new C64DebuggerPluginSpiral();
-//	viewC64->RegisterEmulatorPlugin(plugin);
-
-//	C64DebuggerPluginShowPic *plugin = new C64DebuggerPluginShowPic();
-//	viewC64->RegisterEmulatorPlugin(plugin);
-
 }
 
